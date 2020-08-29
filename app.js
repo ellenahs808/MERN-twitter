308a599734f6b294e4eff1c4124d4d719ee881ec
Shanelle Valencia <sgev808@hawaii.edu>
2020-08-29 12:53:28 -0700

    Update

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const mongoose = require("mongoose");
 const express = require("express");
-const users = require("./routes/api/users");
-const tweets = require("./routes/api/tweets");
-const bodyParser = require("body-parser");
-
-
 const app = express();
 const db = require("./config/keys").mongoURI;
+const mongoose = require("mongoose");
+
+// const users = require("./routes/api/users");
+// const tweets = require("./routes/api/tweets");
+// const bodyParser = require("body-parser");
+
+
 
 
 mongoose
@@ -17,14 +18,15 @@ mongoose
 
 
 app.get("/", (req, res) => {
+    debugger
     res.send("Hello World!");
 });
 
 
-app.use("/api/users", users);
-app.use("/api/tweets", tweets);
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// app.use("/api/users", users);
+// app.use("/api/tweets", tweets);
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 
 
 
@@ -32,5 +34,5 @@ app.use(bodyParser.json());
 const port = process.env.PORT || 5000;
 
 
-app.listen(port, () => (console.log(`Listening on port ${port}`)));
+app.listen(port, () => {console.log(`Listening on port ${port}`)});
 
