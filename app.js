98d5e4f98be8ff05b540b4cf0d135e1835afb5c8
Shanelle Valencia <sgev808@hawaii.edu>
2020-08-29 22:14:17 -0700

    Modify .gitignore

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const db = require("./config/keys").mongoURI;
 const passport = require("passport");
 const User = require("./models/User");
 const bodyParser = require("body-parser");
+const path = require('path');
 
 
 const users = require("./routes/api/users");
@@ -51,3 +52,12 @@ const port = process.env.PORT || 5000;
 
 app.listen(port, () => {console.log(`Listening on port ${port}`)});
 
+
+
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
