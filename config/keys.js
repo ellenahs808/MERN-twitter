81f595d073a9e6418cb5d33d45b1bf804caf67ac
Shanelle Valencia <sgev808@hawaii.edu>
2020-08-31 09:53:34 -0700

    Update config folder

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,3 @@
-// module.exports = {
-//   mongoURI:
-//     "mongodb+srv://dev:YLo6dgJIjwfyrLsM@cluster0.sqwd7.mongodb.net/MERNdb?retryWrites=true&w=majority",
-//     secretOrKey: "secret"
-// };
-
 
 
 if (process.env.NODE_ENV === "production") {
